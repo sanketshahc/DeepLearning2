51e5116cd683f119cb21bf82f3eea81c4df182c1
Sanket Shah <ss4228@cornell.edu>
2021-04-08 22:26:20 +0000

    patch

--- dev.py
+++ dev.py
@@ -692,15 +692,18 @@ def batches_loop(loader, model, criterion, optimizer, is_val=False):
             optimizer.step()
         if batch_count % 250 == 0:
             print(batch_count,' batches complete')
-        
-    return loss_total, y_hat, y, (batch_count * hypes["BATCH"])
+
+    y_hat_arg = y_hat.argmax(dim=-1)
+    count_correct += (y == y_hat_arg).sum()
+    accuracy = (count_correct / (batch_count * hypes["BATCH"]))
+    return loss_total, accuracy
 
 def problem3_1():
     print(hypes)
     cifar_Loader = DataLoader(cifar, drop_last=True, batch_size=hypes['BATCH'])
     cifar_test_Loader = DataLoader(cifar_test,drop_last=True, batch_size=hypes['BATCH'])
     network = TorchNet()
-    # network.to(device)
+    network.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(network.parameters(), lr=hypes["RATE"], momentum=hypes["MOMENTUM"])
     count_epoch = 0
@@ -711,77 +714,74 @@ def problem3_1():
     loss_training = []
     # for epoch in range(2):  # loop over the dataset multiple times
     for epoch in range(hypes["EPOCHS"]):
-        running_loss = 0.0
-        for i, data in enumerate(cifar_Loader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
+        # running_loss = 0.0
+        # for i, data in enumerate(cifar_Loader, 0):
+        #     # get the inputs; data is a list of [inputs, labels]
+        #     inputs, labels = data
 
-            # zero the parameter gradients
-            optimizer.zero_grad()
+        #     # zero the parameter gradients
+        #     optimizer.zero_grad()
 
-            # forward + backward + optimize
-            outputs = network(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+        #     # forward + backward + optimize
+        #     outputs = network(inputs)
+        #     loss = criterion(outputs, labels)
+        #     loss.backward()
+        #     optimizer.step()
 
-            # print statistics
-            running_loss += loss.item()
-            if i % 250 == 0:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss))
-                # running_loss = 0.0
+        #     # print statistics
+        #     running_loss += loss.item()
+        #     if i % 250 == 0:    # print every 2000 mini-batches
+        #         print('[%d, %5d] loss: %.3f' %
+        #             (epoch + 1, i + 1, running_loss))
+        #         # running_loss = 0.0
             
-            correct = 0
-            total = 0
-        # with torch.no_grad():
-        #     for data in cifar_test_Loader:
+        #     correct = 0
+        #     total = 0
+        # # with torch.no_grad():
+        # #     for data in cifar_test_Loader:
             
-            # outputs = network(images)
-            _, predicted = torch.max(outputs.data, -1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-        print(correct, total)
+        #     # outputs = network(images)
+        #     _, predicted = torch.max(outputs.data, -1)
+        #     total += labels.size(0)
+        #     correct += (predicted == labels).sum().item()
+        # # print(correct, total)
 
-        print('Accuracy of the network on the 10000 test images: %d %%' % (
-            100 * correct / total))
+        # print('Accuracy of the network on the 10000 test images: %d %%' % (
+        #     100 * correct / total))
 
     # for epoch in range(hypes["EPOCHS"]):
-        count_correct_training = 0
-        count_correct_testing = 0
         count_epoch+=1
         print('EPOCH:', count_epoch)
         # training
         # network.train()
         training_batches = batches_loop(cifar_Loader, network,criterion,optimizer)
-        y_arg = training_batches[2]
-        print(y_arg.shape, labels.shape)
-        assert (y_arg == labels).sum() == 64, print(y_arg, labels)
-        y_hat_arg = training_batches[1].argmax(dim=-1)
+
+        # print(y_arg.shape, labels.shape)
+        # assert (y_arg == labels).sum() == 64, print(y_arg, labels)
+        # y_hat_arg = training_batches[1].argmax(dim=-1)
         # count_correct_training += (y_arg == y_hat_arg).sum()
         loss_training.append(training_batches[0])
         # _, predicted = torch.max(out.data, -1)
-        assert (training_batches[1] == outputs).sum() == 64, print((training_batches[1] == outputs),training_batches[1].shape, outputs.shape)
-        assert (y_hat_arg == predicted).sum() == 64, print((y_hat_arg == predicted), y_hat_arg, predicted)
+        # assert (training_batches[1] == outputs).sum() == 64, print((training_batches[1] == outputs),training_batches[1].shape, outputs.shape)
+        # assert (y_hat_arg == predicted).sum() == 64, print((y_hat_arg == predicted), y_hat_arg, predicted)
         # assert predicted == 
         # print(predicted)
         # total += labels.size(0)
-        count_correct_training += (predicted == y_arg).sum().item()
+        # count_correct_training += (predicted == y_arg).sum().item()
     #     # testing
     #     network.eval()
-    #     testing_batches = batches_loop(cifar_test_Loader,network, criterion,optimizer, True)
-    #     yt_arg = testing_batches[2]
-    #     yt_hat_arg = testing_batches[1].argmax(dim=-1)
-    #     count_correct_testing += (yt_arg == yt_hat_arg).sum()
-    #     loss_testing.append(testing_batches[0])
-        accuracy = (count_correct_training / training_batches[3])
-    #     accuracy_test = (count_correct_testing / len(cifar_test)).item()
-        print('training_loss', training_batches[0], 'cprrect', count_correct_training,'accuracy', accuracy)
-        
-    #     print('testing_loss', testing_batches[0], 'accuracy_test', accuracy_test)
-        #     print('correc_training', count_correct_training, 'correct_testing', count_correct_testing)
-        #     print('out of', len(cifar), len(cifar_test))
-        # metrics = (loss_training,loss_testing,accuracy,accuracy_test)
+        testing_batches = batches_loop(cifar_test_Loader,network, criterion,optimizer, True)
+        #     yt_arg = testing_batches[2]
+        #     yt_hat_arg = testing_batches[1].argmax(dim=-1)
+        #     count_correct_testing += (yt_arg == yt_hat_arg).sum()
+        loss_testing.append(testing_batches[0])
+        accuracy = training_batches[1]
+        accuracy_test = testing_batches[1]
+        print('training_loss', training_batches[0], 'accuracy', accuracy)
+        print('testing_loss', testing_batches[0], 'accuracy_test', accuracy_test)
+    # print('correc_training', count_correct_training, 'correct_testing', count_correct_testing)
+    # print('out of', len(cifar), len(cifar_test))
+    metrics = (loss_training,loss_testing,accuracy,accuracy_test)
         # save_bin(f'{FILE}', network)
     # save_bin(f'{FILE}_metrics', metrics)
     
