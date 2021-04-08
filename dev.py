b2cf1e101490a0899fa98ca6d80bb2d4602a5f05
Sanket Shah <ss4228@cornell.edu>
2021-04-08 20:54:48 +0000

    patch

--- dev.py
+++ dev.py
@@ -726,7 +726,7 @@ def problem3_1():
 
             # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
+            if i % 250 == 0:    # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
                     (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
