b7444f0f04eb7964974cc7ecf618a80fe16a6e9e
Sanket Shah <ss4228@cornell.edu>
2021-04-11 18:40:20 +0000

    patch

--- dev.py
+++ dev.py
@@ -594,7 +594,7 @@ def filter_visual(Model):
     plt.imshow(tiles, interpolation='bilinear')
     plt.show()
     assert(type(stamp)== int), stamp
-    # plt.savefig(f'./plots/filt_{FILE}{stamp}.png')
+    plt.savefig(f'./plots/filt_{FILE}{stamp}.png')
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
